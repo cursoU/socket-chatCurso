39a817047b4c72d75f67844f87d1707f3be751e5
Joe <zx_pato@hotmail.com>
2018-12-04 21:38:09 -0300

    first commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,74 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El Nombre y sala son necesario');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
+socket.on('connect', function() {
+
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+
+    });
+
+
+
+
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+/* // Enviar información
+socket.emit('crearMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+}); */
+
+
+
+
+
+
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+
+
+// Escuchar usuario cuando entra o sale
+socket.on('listaPersona', function(personas) {
+
+    console.log(personas);
+
+});
+
+
+
+//mensajes Privados
+
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado:', mensaje);
+});
